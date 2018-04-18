2d963f6a2a8459d20979f1ca9bb324ecd0332e58
milad <m.rezai2724@yahoo.com>
2018-04-18 16:03:50 +0430

    add retrofit to project

--- app/src/main/java/com/example/milad/simple_app_android/MainActivity.java
+++ app/src/main/java/com/example/milad/simple_app_android/MainActivity.java
@@ -2,6 +2,12 @@ package com.example.milad.simple_app_android;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+
+import com.backtory.java.HttpStatusCode;
+import com.backtory.java.internal.BacktoryCallBack;
+import com.backtory.java.internal.BacktoryResponse;
+import com.backtory.java.internal.BacktoryUser;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +15,7 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+
     }
 }
